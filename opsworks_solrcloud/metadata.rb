b7c7c6d48f5d670ece97329ed48fdcba59cfb2cd
timo.schmidt <timo-schmidt@gmx.net>
2014-09-25 12:17:34 +0200

    Remove solrcloud dependency for now

--- opsworks_solrcloud/metadata.rb
+++ opsworks_solrcloud/metadata.rb
@@ -5,7 +5,6 @@ license 	""
 version 	"0.0.1"
 
 depends 	"deploy"
-depends		"solrcloud"
 
 recipe		"opsworks_solrcloud::install", "Installs solrcloud as opsworks layer"
 recipe		"opsworks_solrcloud::setup", "Setting up solr cloud on opsworks"
