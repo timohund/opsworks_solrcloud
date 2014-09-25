9e2faf448390b245f26e6f7a3a4ef848edc23b1d
timo.schmidt <timo-schmidt@gmx.net>
2014-09-25 14:32:14 +0200

    Changed to log with info level

--- opsworks_solrcloud/recipes/install.rb
+++ opsworks_solrcloud/recipes/install.rb
@@ -1,6 +1,1 @@
-message = node.has_key?(:message) ? node[:message] : "Hello World"
-
-execute "echo message" do
-  command "echo '#{message}' opsworks_solrcloud install"
-  action :run
-end
+Chef::Log.info("Running opsworks solrcloud setup")
