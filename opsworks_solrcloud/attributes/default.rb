af7d0c3ed7361546925a99d3e0499c3360bae22f
Timo Schmidt <timo-schmidt@gmx.net>
2014-09-29 18:00:14 +0200

    Use localhost as exibitor

--- opsworks_solrcloud/attributes/default.rb
+++ opsworks_solrcloud/attributes/default.rb
@@ -2,4 +2,4 @@ include_attribute 'solrcloud'
 
 node.set['solrcloud']['zk_run'] =  false
 
-default['opsworks_solrcloud']['zookeeper_exibitor_uri'] = "#{node['opsworks']['layers']['solrcloud']['instances']['master']['private_dns_name']}"
+default['opsworks_solrcloud']['zookeeper_exibitor_uri'] = 'http://127.0.0.1:8080/'
