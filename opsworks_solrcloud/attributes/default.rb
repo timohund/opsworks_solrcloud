97010bcaff0ecb0bdefd9be3dfe2eed2783430c6
timo.schmidt <timo-schmidt@gmx.net>
2014-09-29 11:25:07 +0200

    Added dependency to zookeeper cookbook Install zookeeper in setup.rb

--- opsworks_solrcloud/attributes/default.rb
+++ opsworks_solrcloud/attributes/default.rb
@@ -1,3 +1,4 @@
 include_attribute 'solrcloud'
 
-node.set['solrcloud']['zk_run'] =  true
+node.set['solrcloud']['zk_run'] =  false
+node.set['solrcloud']['solr_config']['solrcloud']['zk_host'] = discover_zookeepers(#{node['opsworks_solrcloud']['zookeeper_exhibitor_url']})
--- opsworks_solrcloud/recipes/setup.rb
+++ opsworks_solrcloud/recipes/setup.rb
@@ -1,3 +1,4 @@
 Chef::Log.info("Running opsworks solrcloud setup")
 
+include_recipe 'zookeeper::install'
 include_recipe 'solrcloud::tarball'
