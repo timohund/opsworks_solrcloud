c53a73f8336fc9fd91faab48724d38aeed77d5ed
Timo Schmidt <timo-schmidt@gmx.net>
2014-09-29 16:23:00 +0200

    Fixed attribute

--- opsworks_solrcloud/attributes/default.rb
+++ opsworks_solrcloud/attributes/default.rb
@@ -2,5 +2,5 @@ include_attribute 'solrcloud'
 
 node.set['solrcloud']['zk_run'] =  false
 
-default['opsworks_solrcloud']['zookeeper_exibitor_uri'] = "#{node['opsworks']['stack']['elb-load-balancers']['dns_name']}"
+default['opsworks_solrcloud']['zookeeper_exibitor_uri'] = "#{node['opsworks']['stack']['elb-load-balancers'][0]['dns_name']}"
 
