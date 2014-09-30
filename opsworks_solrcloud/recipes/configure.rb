bc7547dcc897c5adf460d3cd76e5c238f91c5ac0
Timo Schmidt <timo-schmidt@gmx.net>
2014-09-30 17:23:05 +0200

    Added protocol and port

--- opsworks_solrcloud/recipes/configure.rb
+++ opsworks_solrcloud/recipes/configure.rb
@@ -3,7 +3,7 @@ Chef::Log.info("Running opsworks solrcloud configure")
 include_recipe 'exhibitor::default'
 
 opsworks_solrcloud "solr cloud" do
-  public_dns_name node['opsworks']['layers']['solrcloud']['instances']['solrcloud1']['public_dns_name']
+  public_dns_name "http://#{node['opsworks']['layers']['solrcloud']['instances']['solrcloud1']['public_dns_name']}:8080/"
   force_exibitor_uri node['opsworks_solrcloud']['zookeeper']['exibitor']['url']
   use_first_node node['opsworks_solrcloud']['zookeeper']['exibitor']['use_first_node']
 end
