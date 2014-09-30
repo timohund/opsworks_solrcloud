ba27b88dfbf2bfa02ed6923294b2d30acfbfe36a
Timo Schmidt <timo-schmidt@gmx.net>
2014-09-30 17:11:11 +0200

    Fixed datatype in resource provider

--- opsworks_solrcloud/attributes/default.rb
+++ opsworks_solrcloud/attributes/default.rb
@@ -1,6 +1,9 @@
 include_attribute 'solrcloud'
-
 node.set['solrcloud']['zk_run'] =  false
+
+#include_attribute 'exhibitor'
+#node.override['exhibitor']['install_method'] = 'download'
+#node.override['exhibitor']['mirror'] = 'http://central.maven.org/maven2/com/netflix/exhibitor/exhibitor-standalone/1.5.0/exhibitor-standalone-1.5.0.jar'
 
 default['opsworks_solrcloud']['zookeeper']['exibitor']['use_first_node'] = true
 default['opsworks_solrcloud']['zookeeper']['exibitor']['url'] = 'http://127.0.0.1:8080/'
--- opsworks_solrcloud/resources/default.rb
+++ opsworks_solrcloud/resources/default.rb
@@ -3,4 +3,4 @@ default_action(:install)
 
 attribute :public_dns_name,     kind_of: String, name_attribute: true
 attribute :force_exibitor_uri,   kind_of: String
-attribute :use_first_node,       kind_of: Boolean
+attribute :use_first_node,      kind_of: [TrueClass, FalseClass], default: true
