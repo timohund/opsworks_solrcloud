b61e39bfcc75a65341f18c7fdcac6040ff340a34
Timo Schmidt <timo-schmidt@gmx.net>
2014-09-30 18:13:46 +0200

    Add service recipie call

--- opsworks_solrcloud/recipes/configure.rb
+++ opsworks_solrcloud/recipes/configure.rb
@@ -2,6 +2,9 @@ Chef::Log.info("Running opsworks solrcloud configure")
 
 include_recipe 'exhibitor::default'
 
+include_recipe 'zookeeper::service'
+include_recipe 'exhibitor::service'
+
 opsworks_solrcloud "solr cloud" do
   public_dns_name "http://#{node['opsworks']['layers']['solrcloud']['instances']['solrcloud1']['public_dns_name']}:8080/"
   force_exibitor_uri node['opsworks_solrcloud']['zookeeper']['exibitor']['url']
