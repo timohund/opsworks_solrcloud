06df42c1ba7311ccdff633d13a948d91683fe466
Timo Schmidt <timo-schmidt@gmx.net>
2014-09-30 13:44:18 +0200

    Fixed dependencies and cookbook name

--- opsworks_solrcloud/attributes/default.rb
+++ opsworks_solrcloud/attributes/default.rb
@@ -2,9 +2,5 @@ include_attribute 'solrcloud'
 
 node.set['solrcloud']['zk_run'] =  false
 
-include_attribute 'zookeeper'
-
-node.set['zookeeper']['version'] = "3.4.6"
-
 default['opsworks_solrcloud']['zookeeper']['exibitor']['use_first_node'] = true
 default['opsworks_solrcloud']['zookeeper']['exibitor']['url'] = 'http://127.0.0.1:8080/'
--- opsworks_solrcloud/metadata.rb
+++ opsworks_solrcloud/metadata.rb
@@ -7,6 +7,7 @@ version 	"0.0.1"
 depends 	"deploy"
 depends		"solrcloud"
 depends     "zookeeper"
+depends     "exhibitor"
 
 recipe		"opsworks_solrcloud::configure", "Installs solrcloud as opsworks layer"
 recipe		"opsworks_solrcloud::setup", "Setting up solr cloud on opsworks"
