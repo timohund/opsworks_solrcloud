fee2f712cb980a15e6104e694dec01f2fc598016
Timo Schmidt <timo-schmidt@gmx.net>
2014-10-07 17:38:29 +0200

    Use first node

--- opsworks_solrcloud/recipes/configure.rb
+++ opsworks_solrcloud/recipes/configure.rb
@@ -1,6 +1,6 @@
 Chef::Log.info("First node is #{node['opsworks']['layers']['solrcloud']['instances'].first}")
 
-firsthost = node['opsworks']['layers']['solrcloud']['instances'].first.last
+firsthost = node['opsworks']['layers']['solrcloud']['instances'].first.first
 
 Chef::Log.info("First node is #{firsthost['private_dns_name']}")
 
