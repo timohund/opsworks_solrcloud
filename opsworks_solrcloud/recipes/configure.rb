47fe9bdc41315f6932434bf39ca0f1f60fd27e50
Timo Schmidt <timo-schmidt@gmx.net>
2014-10-07 15:42:28 +0200

    Seperated setup and configure

--- opsworks_solrcloud/recipes/configure.rb
+++ opsworks_solrcloud/recipes/configure.rb
@@ -1,15 +1,9 @@
-Chef::Log.info("Running opsworks solrcloud configure")
-
-include_recipe 'exhibitor::default'
-
-include_recipe 'runit'
-include_recipe 'zookeeper::service'
-include_recipe 'exhibitor::service'
-
 Chef::Log.info("First node is #{node['opsworks']['layers']['solrcloud']['instances'].first}")
 
-firsthost = node['opsworks']['layers']['solrcloud']['instances'].first
+firsthost = node['opsworks']['layers']['solrcloud']['instances'].first.last
+
+Chef::Log.info("First node is #{firsthost['private_dns_name']}")
 
 opsworks_solrcloud "solr cloud" do
-  exhibitor_uri "http://#{firsthost['public_dns_name']}:8080/"
+  exhibitor_uri "http://#{firsthost['private_dns_name']}:8080/"
 end
--- opsworks_solrcloud/recipes/setup.rb
+++ opsworks_solrcloud/recipes/setup.rb
@@ -1,3 +1,6 @@
 Chef::Log.info("Running opsworks solrcloud setup")
+include_recipe 'exhibitor::default'
 
-
+include_recipe 'runit'
+include_recipe 'zookeeper::service'
+include_recipe 'exhibitor::service'
