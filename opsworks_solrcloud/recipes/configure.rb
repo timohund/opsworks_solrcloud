e78540da1b19a4a6807183f923178056e96e4304
Timo Schmidt <timo-schmidt@gmx.net>
2014-10-07 14:28:18 +0200

    Move to configure

--- opsworks_solrcloud/recipes/configure.rb
+++ opsworks_solrcloud/recipes/configure.rb
@@ -1,1 +1,11 @@
 Chef::Log.info("Running opsworks solrcloud configure")
+
+include_recipe 'exhibitor::default'
+
+include_recipe 'runit'
+include_recipe 'zookeeper::service'
+include_recipe 'exhibitor::service'
+
+opsworks_solrcloud "solr cloud" do
+  exhibitor_uri "http://#{node['opsworks']['layers']['solrcloud']['instances'][0]['public_dns_name']}:8080/"
+end
