b58af927c8b919b11fa3cc1eae4c058acd368173
Timo Schmidt <timo-schmidt@gmx.net>
2014-10-09 17:33:41 +0200

    Fixed wrong resource name

--- opsworks_solrcloud/recipes/deploy.rb
+++ opsworks_solrcloud/recipes/deploy.rb
@@ -1,4 +1,4 @@
 Chef::Log.info("Running opsworks solrcloud deploy")
 
-opsworks_solrcloud_setup_deploy_solr_configuration "Deploying solr configuration"
+opsworks_solrcloud_deploy_solr_configuration "Deploying solr configuration"
 
