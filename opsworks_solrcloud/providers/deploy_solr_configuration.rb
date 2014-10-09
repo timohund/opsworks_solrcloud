9fe3721e91b89570cbc1ca9d0ea85cf2015af4bf
Timo Schmidt <timo-schmidt@gmx.net>
2014-10-09 18:08:04 +0200

    Chef i provider if configuration is given

--- opsworks_solrcloud/providers/deploy_solr_configuration.rb
+++ opsworks_solrcloud/providers/deploy_solr_configuration.rb
@@ -1,22 +1,29 @@
 action :deploy do
-    firsthost = node['opsworks']['layers']['solrcloud']['instances'].first[1]
 
-    node.set['opsworks_solrcloud']['is_first_cluster_node'] = firsthost['private_ip'] == node['ipaddress']
-    Chef::Log.info("Is this the first node in the cluster?: #{node['opsworks_solrcloud']['is_first_cluster_node']}")
+    if node['opsworks']['layers']['solrcloud']['instances'].first.nil?
+        Chef::Log.info("No first instance for layer solrcloud available skipping deployment")
+    else
+        Chef::Log.info("Starting deployment of solr configuration")
 
-    # Disable the embedded zookeeper in solr
-    node.set['solrcloud']['zk_run'] = false
+        firsthost = node['opsworks']['layers']['solrcloud']['instances'].first[1]
 
-    # configset updates should only be triggered on the first cluster node
-    node.set['solrcloud']['manage_zkconfigsets'] = node['opsworks_solrcloud']['is_first_cluster_node']
+        node.set['opsworks_solrcloud']['is_first_cluster_node'] = firsthost['private_ip'] == node['ipaddress']
+        Chef::Log.info("Is this the first node in the cluster?: #{node['opsworks_solrcloud']['is_first_cluster_node']}")
 
-    # we want to put the configSets by our own
-    node.set['solrcloud']['manage_zkconfigsets_source'] = false
+        # Disable the embedded zookeeper in solr
+        node.set['solrcloud']['zk_run'] = false
 
-    # collections should only be managed on the first cluster node
-    node.set['solrcloud']['manage_collections'] = node['opsworks_solrcloud']['is_first_cluster_node']
+        # configset updates should only be triggered on the first cluster node
+        node.set['solrcloud']['manage_zkconfigsets'] = node['opsworks_solrcloud']['is_first_cluster_node']
 
-    run_context.include_recipe "solrcloud::zkconfigsets"
+        # we want to put the configSets by our own
+        node.set['solrcloud']['manage_zkconfigsets_source'] = false
 
-    run_context.include_recipe "solrcloud::collections"
+        # collections should only be managed on the first cluster node
+        node.set['solrcloud']['manage_collections'] = node['opsworks_solrcloud']['is_first_cluster_node']
+
+        run_context.include_recipe "solrcloud::zkconfigsets"
+
+        run_context.include_recipe "solrcloud::collections"
+    end
 end
--- opsworks_solrcloud/resources/setup_solr.rb
+++ opsworks_solrcloud/resources/setup_solr.rb
@@ -1,4 +1,2 @@
 actions(:setup)
 default_action(:setup)
-
-attribute :exhibitor_uri, kind_of: String, name_attribute: true
