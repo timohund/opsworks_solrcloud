629bc2f81791ee398b7be5d2ed500883264b104d
Timo Schmidt <timo-schmidt@gmx.net>
2014-10-09 13:30:13 +0200

    Trigger zkconfigsets and collections in deploy recepie

--- opsworks_solrcloud/recipes/deploy.rb
+++ opsworks_solrcloud/recipes/deploy.rb
@@ -1,1 +1,7 @@
 Chef::Log.info("Running opsworks solrcloud deploy")
+
+# Setup configsets - node['solrcloud']['zkconfigsets']
+include_recipe "solrcloud::zkconfigsets"
+
+# Setup collections - node['solrcloud']['collections']
+include_recipe "solrcloud::collections"
