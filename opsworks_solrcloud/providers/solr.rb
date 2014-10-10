62572dca6b0455e1e8b0af588b4f00d6cec0fb89
Timo Schmidt <timo-schmidt@gmx.net>
2014-10-10 17:19:32 +0200

    Create configset home if not exists

--- opsworks_solrcloud/providers/solr.rb
+++ opsworks_solrcloud/providers/solr.rb
@@ -111,6 +111,13 @@ action :getconfig do
       action :create
     end
 
+    directory {node['solrcloud']['zkconfigsets_home'] do
+      owner 'root'
+      group 'root'
+      mode '0644'
+      action :create
+    end
+
     aws_s3_file "#{zkconfigtar_tmp}solrconfig.tar.gz" do
       bucket new_resource.zkconfigsets_s3_bucket
       remote_path new_resource.zkconfigsets_s3_remote_path
