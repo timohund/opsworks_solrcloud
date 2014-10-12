f904a4f085d366a4159227b8863457ffdaee623b
Timo Schmidt <timo-schmidt@gmx.net>
2014-10-12 11:28:52 +0200

    Use solrcloud::tarball_deployconfig for deployment

--- opsworks_solrcloud/recipes/deploy.rb
+++ opsworks_solrcloud/recipes/deploy.rb
@@ -1,9 +1,13 @@
 Chef::Log.info("Running opsworks solrcloud deploy")
 
-opsworks_solrcloud_solr "Deploying solr configuration" do
-    action :getconfig
-end
+if node['opsworks']['layers']['solrcloud']['instances'].first.nil?
+    Chef::Log.info("No first instance for layer solrcloud available skipping deployment")
+else
+    opsworks_solrcloud_solr "Deploying solr configuration" do
+        action :getconfig
+    end
 
-opsworks_solrcloud_solr "Deploying solr configuration" do
-    action :deployconfig
+    opsworks_solrcloud_solr "Deploying solr configuration" do
+        action :deployconfig
+    end
 end
