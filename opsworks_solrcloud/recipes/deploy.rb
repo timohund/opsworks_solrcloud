803c9e5a0851e5639e5ab5c21367fb04cd1bb8fa
Timo Schmidt <timo-schmidt@gmx.net>
2014-10-13 10:53:29 +0200

    Only deploy the configuration from the first cluster node

--- opsworks_solrcloud/recipes/deploy.rb
+++ opsworks_solrcloud/recipes/deploy.rb
@@ -3,11 +3,15 @@ Chef::Log.info("Running opsworks solrcloud deploy")
 if node['opsworks']['layers']['solrcloud']['instances'].first.nil?
     Chef::Log.info("No first instance for layer solrcloud available skipping deployment")
 else
-    opsworks_solrcloud_solr "Deploying solr configuration" do
-        action :getconfig
-    end
+    firsthost = node['opsworks']['layers']['solrcloud']['instances'].first[1]
+    #only run on the first cluster node
+    if firsthost['private_ip'] == node['ipaddress']
+        opsworks_solrcloud_solr "Deploying solr configuration" do
+            action :getconfig
+        end
 
-    opsworks_solrcloud_solr "Deploying solr configuration" do
-        action :deployconfig
+        opsworks_solrcloud_solr "Deploying solr configuration" do
+            action :deployconfig
+        end
     end
 end
