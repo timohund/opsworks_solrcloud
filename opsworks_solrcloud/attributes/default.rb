c72d1d22a36c0abc4f22bfa92ae4be77ba84d1e3
Timo Schmidt <timo-schmidt@gmx.net>
2014-10-24 18:21:59 +0200

    Overwrite admin_path

--- opsworks_solrcloud/attributes/default.rb
+++ opsworks_solrcloud/attributes/default.rb
@@ -1,7 +1,9 @@
-include_attribute 'solrcloud'
 default['opsworks_solrcloud']['zkconfigsets']['source'] = 's3'
 default['opsworks_solrcloud']['zkconfigsets']['s3']['bucket'] = 'mybucket'
 default['opsworks_solrcloud']['zkconfigsets']['s3']['remote_path'] = 'mybucket_path'
 default['opsworks_solrcloud']['zkconfigsets']['s3']['aws_access_key_id'] = 'mybucket_access_key_id'
 default['opsworks_solrcloud']['zkconfigsets']['s3']['aws_secret_access_key'] = 'myaccess_secet_access_key'
+
+include_attribute 'solrcloud'
 node.set['solrcloud']['jetty_config']['context']['path'] = '/'
+node.set['solrcloud']['solr_config']['admin_path'] = '/admin'
